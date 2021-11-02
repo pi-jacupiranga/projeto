ccfc6e088016fd54f774f7f9e7374ed9394e4556
gbardusco <bardusco.gustavo@gmail.com>
2021-11-02 19:05:05 -0300

    Adicionada listagem de usuários com paginação

--- resources/views/layouts/dashboard/users.blade.php
+++ resources/views/layouts/dashboard/users.blade.php
@@ -11,6 +11,44 @@
     @can('is-admin')
       
         <h1>Usuários</h1>
+        
+        <table class="table">
+
+            <thead>
+
+                <tr>
+                    <th>Nome</th>
+                    <th>Setor</th>
+                    <th>Ações</th>
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->name }} {{ $user->surname }}</td>
+                        <td>
+                            @foreach ($setores as $setor)
+                                @if($setor->id == $user->setor_id)
+                                    {{ $setor->setor_nome }}
+                                @endif
+                            @endforeach    
+                        </td>
+                        <td></td>
+                    </tr>    
+
+                @endforeach
+
+            </tbody>
+
+        </table>
+        
+        <div class="d-flex justify-content-center">
+            {{ $users->links() }}
+        </div>
+        
     
     @else
         
